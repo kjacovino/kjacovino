083a60ad030026d252805233c54ec9e424aa633e
Kristi-Lynn Jacovino <kristi.lynn@gmail.com>
2019-09-27 17:54:53 -0400

    really no js

--- js/scripts.js
+++ js/scripts.js
@@ -16,15 +16,15 @@
 //
 // });
 
-$(function () {
-    $("#nav").load("/templates/menu.html");
-});
-
-jQuery(function($) {
- var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
- $('ul a').each(function() {
-  if (this.href === path) {
-   $(this).addClass('active');
-  }
- });
-});
+// $(function () {
+//     $("#nav").load("/templates/menu.html");
+// });
+//
+// jQuery(function($) {
+//  var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+//  $('ul a').each(function() {
+//   if (this.href === path) {
+//    $(this).addClass('active');
+//   }
+//  });
+// });
